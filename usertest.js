280e63fe4151e2a988be44aba88ccfe62fb25e8e
Nathan Breit <nabreit@gmail.com>
2014-08-26 15:09:55 +0000

    Adding conditions, loading message, additional tasks/articles, and article pages

--- usertest.js
+++ usertest.js
@@ -3,15 +3,17 @@ Submissions = new Meteor.Collection("submissions");
 Users = new Meteor.Collection("users");
 Articles = new Meteor.Collection("articles");
 var tasks = [
-  "Identify the diseases mentioned in an article.",
-  "Make predictions about where the next reports of a disease are likely to come from, when they will occur, how many cases will be reported, and who will be infected.",
-  "Identify outbreak risk factors based on the information present in an article.",
-  "Determine the number of cases reported in an article.",
-  "Identify new information in an article (e.g. case counts, locations) that wasn't present in previously shown articles."
+  "Identify the disease(s) reported on in the article.",
+  "Who contracted the disease(s) reported?",
+  "Was there something novel or unusual about the infections reported?",
+  "Which geographic regions are at risk for experiencing outbreaks of the reported disease(s)?",
+  "Which are probable risk factors for contracting the reported disease(s)?",
+  "Which demographics are at risk for the reported disease(s)?",
+  "Estimate the number of infections that will occur in the next 30 days after the article's publication."
 ]
 
 Router.map(function() {
-  this.route('', {
+  this.route('main', {
     path: '/',
     onRun: function(){
       if (!('user' in this.params)) {
@@ -30,15 +32,26 @@ Router.map(function() {
       } else {
         Session.set('userId', user._id);
       }
-      if('showDashboard' in this.params) {
-        Session.set('showDashboard', true);
-      }
+      Session.set('condition', 'a');
+    }
+  });
+  this.route('showArticle', {
+    path: '/showArticle/:idx',
+    data: function(){
+      console.log(this.params);
+      return Articles.findOne({'idx' : Number(this.params.idx)});
     }
   });
 });
 
 if (Meteor.isClient) {
   var taskStart = new Date();
+  
+  var showDashboard = function () {
+    var user = Users.findOne(Session.get('userId'));
+    if(!user) return null;
+    return user.articleIdx % 3 === Session.get('condition');
+  };
   
   Template.main.task = function () {
     var user = Users.findOne(Session.get('userId'));
@@ -49,23 +62,28 @@ if (Meteor.isClient) {
   Template.main.events({
     'click .next': function () {
       if(confirm("Go to next task?")) {
+        var user = Users.findOne(Session.get('userId'));
         //TODO: Submit
         Submissions.insert({
           userId : Session.get('userId'),
+          condition : Session.get('condition'),
           submissionTime : new Date(),
+          showDashboard : showDashboard(),
           taskStart : taskStart,
+          taskIdx : user.taskIdx,
+          articleIdx : user.articleIdx,
           taskResult : $('#task-result').val()
         });
-        $('#task-result').val('')
+        $('#task-result').val('');
         taskStart = new Date();
-        Users.update(Session.get('userId'), {$inc: { taskIdx : 1 }})
+        Users.update(Session.get('userId'), {$inc: { taskIdx : 1 }});
       }
     },
     'click .next-article': function () {
       Users.update(Session.get('userId'), {
         $inc: { articleIdx : 1 },
         $set: { taskIdx : 0 }
-      })
+      });
     }
   });
 
@@ -74,19 +92,26 @@ if (Meteor.isClient) {
     if(!user) return null;
     return Articles.findOne({'idx' : user.articleIdx});
   };
-  Template.main.showDashboard = function () {
-    return Session.get('showDashboard');
+  
+  Template.main.completed = function () {
+    var user = Users.findOne(Session.get('userId'));
+    if(!user) return false;
+    return Articles.find({
+      $in : [user.articleIdx, user.articleIdx - 1]
+    }).count() === 1;
   };
+  
+  Template.main.showDashboard = showDashboard;
 }
 
 if (Meteor.isServer) {
   var fs = Npm.require('fs');
   var directory = "../../../../../articles/";
+  Articles.remove({});
   fs.readdirSync(directory, 'utf8').forEach(function(filename){
     try {
       var data = YAML.safeLoad(fs.readFileSync(directory + filename, 'utf8'));
       console.log(data);
-      Articles.remove({});
       Articles.insert(data);
     } catch (e) {
       console.log(e);
